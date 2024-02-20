4330a985b71b9c3b0e3bea7bac8f1e9631322760
Heitorrk <134640990+Heitorrk@users.noreply.github.com>
2024-02-20 10:13:10 -0300

    Aula 1

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		String nome = "Heitor";
+		System.out.println("Bom dia " + nome);
+		System.out.println(nome + " chegamos ao \n final da aula");
+	}
+	
+	public static void go() {
+		System.out.println("To indo..");
+	}
+
+}
